076964f329d726b546656b85c26359d99728eb06
Skylar Steinb <skylar@peringer.com>
2016-04-26 20:25:24 -0700

    * install_dependencies.js -> install-dependencies.js * add 'linuxDependencies' to package.json. install-dependencies.js will install these modules on Linux platforms

---
+++ install-dependencies.js
@@ -0,0 +1,28 @@
+/*
+  install-dependencies.js
+
+  We have some hardware-dependent npm modules for device access on the Raspberry Pi that
+  do not compile on all platforms. This script is called during the 'npm install' process
+  and will only install the 'linuxDependencies' modules on the target platform. Yes, I'm
+  laying on a bit of anti-pattern on here since failures are not bubbled up.
+*/
+
+const os           = require('os');
+const path         = require('path');
+const packageJson  = require(path.join(__dirname, 'package.json'));
+const execSync     = require('child_process').execSync;
+const colors       = require('colors/safe');
+
+if(os.type() === 'Linux' && packageJson.linuxDependencies) {
+
+  for(var pkg of Object.keys(packageJson.linuxDependencies)) {
+
+    var cmd = 'npm install ' + pkg + '@' + packageJson.linuxDependencies[pkg];
+    console.log(colors.cyan.bold('exec(\'' + cmd + '\')'));
+
+    var result = execSync(cmd, { encoding: 'utf8' });
+
+    console.log(colors.black.bgYellow(cmd));
+    console.log(result);
+  }
+}
