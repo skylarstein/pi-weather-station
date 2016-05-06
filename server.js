17ef55f5f2c72c5fc2d3f37feed6054d9693cced
Skylar Stein <skylar@peringer.com>
2016-05-05 22:58:08 -0700

    error handling

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /*
   server.js
-
   The HTTP server for our Raspberry Pi Weather Station
 */
 
@@ -10,15 +9,44 @@ const app        = require('./app.js');
 const os         = require('os');
 const http       = require('http');
 const appPackage = require('./package.json');
+const port       = process.env.PORT || 8888;
 
 // Start our server
 //
-const httpServer = http.createServer(app).listen(process.env.PORT || 8888);
+const httpServer = http.createServer(app).listen(port);
 
 httpServer.on('listening', () => {
   console.log(`Welcome to ${appPackage.description} v${appPackage.version}`);
   console.log(`Node ${process.version}, ${os.platform()}/${os.arch()}`);
-  console.log(`HTTP server listening on port ${httpServer.address().port} on ${os.hostname()}`);
+
+  const addr = httpServer.address();
+  const bind = (typeof addr === 'string') ? 'pipe ' + addr : 'port ' + addr.port;
+
+  console.log(`HTTP server listening on ${bind} on ${os.hostname()}`);
+});
+
+httpServer.on('error', error => {
+  if(error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
+
+  switch(error.code) {
+
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+
+    default:
+      throw error;
+  }
 });
 
 // Clean shutdown
