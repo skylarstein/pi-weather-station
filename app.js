418c0c0139a56f0c5b5643dd87adcfaadf9a6df1
Skylar Stein <skylar@peringer.com>
2016-11-10 22:50:18 -0800

    server.js module as class, app.js cleanup

--- app.js
+++ app.js
@@ -17,36 +17,40 @@ class WeatherStationApp {
   constructor() {
     this.express = express();
 
-    const _initMiddleware = () => {
-      this.express.use(cors()); // easier front end testing from local dev machine with CORS enabled
-      this.express.use(express.static(`${__dirname}/public`)); // serve up static assets
-    };
-
-    const _initRoutes = () => {
-      this.express.use('/', routes); // mount app routes
-    };
-
-    const _connectDatabase = () => {
-      mongoose.set('debug', true);
-      mongoose.Promise = global.Promise; // Mongoose 5.0 will use native promises. Until then explicitly request it.
-
-      mongoose.connection.on('connected', () => console.log('Connected to database'));
-      mongoose.connection.on('disconnected', () => console.log('Disconnected from database'));
-      mongoose.connection.on('error', (err) => console.error(`MongoDB error: ${err}`));
-
-      mongoose.connect(process.env.DB_URL, (err) => {
-        if(err) {
-          console.error(`mongoose.connect error: ${err}`);
-        }
-      });
-
-      DatabasePublisher.instance().startPublishing();
-    };
-
-    _initMiddleware();
-    _initRoutes();
-    _connectDatabase();
+    this._initMiddleware();
+    this._initRoutes();
+    this._connectDatabase();
   }
+
+  // For the sake of clean code I'm not going out of my way to make these private
+  // beyond suggesting that they are private with the underscore-prefix naming convention.
+  //
+  _initMiddleware() {
+    this.express.use(cors()); // easier front end testing from local dev machine with CORS enabled
+    this.express.use(express.static(`${__dirname}/public`)); // serve up static assets
+  }
+
+  _initRoutes() {
+    this.express.use('/', routes); // mount app routes
+  }
+
+  _connectDatabase() {
+    mongoose.set('debug', true);
+    mongoose.Promise = global.Promise; // Mongoose 5.0 will use native promises. Until then explicitly request it.
+
+    mongoose.connection.on('connected', () => console.log('Connected to database'));
+    mongoose.connection.on('disconnected', () => console.log('Disconnected from database'));
+    mongoose.connection.on('error', (err) => console.error(`MongoDB error: ${err}`));
+
+    mongoose.connect(process.env.DB_URL, (err) => {
+      if(err) {
+        console.error(`mongoose.connect error: ${err}`);
+      }
+    });
+
+    DatabasePublisher.instance().startPublishing();
+  }
+
 }
 
-module.exports = new WeatherStationApp().express;
+module.exports = WeatherStationApp;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*
   server.js
-  The HTTP server for our Raspberry Pi Weather Station
+
+  The Node.js HTTP server for our Raspberry Pi Weather Station
 */
 
 'use strict';
@@ -9,70 +10,93 @@ if(!require('dotenv').config({silent: true})) {
   //throw new Error('An .env file is required!');
 }
 
-const app                = require('./app.js');
+const WeatherStationApp  = require('./app.js');
 const os                 = require('os');
 const http               = require('http');
 const appPackage         = require('./package.json');
 const port               = process.env.PORT || 8888;
 const WebSocketPublisher = require('./controllers/publisher-ws.js');
 
-// Start our server
-//
-const httpServer = http.createServer(app).listen(port);
+class WeatherStationServer {
 
-httpServer.on('listening', () => {
-  console.log(`Welcome to ${appPackage.description} v${appPackage.version}`);
-  console.log(`Node ${process.version}, ${os.platform()}/${os.arch()}`);
+  constructor() {
+    this.app = new WeatherStationApp().express;
 
-  const addr = httpServer.address();
-  const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
-
-  console.log(`HTTP server listening on ${bind} on ${os.hostname()}`);
-
-  WebSocketPublisher.instance().startPublishing(httpServer);
-});
-
-httpServer.on('error', (error) => {
-  if(error.syscall !== 'listen') {
-    throw error;
+    this._initServer();
+    this._startWebSocketPublisher();
+    this._initCleanShutdownHandler();
   }
 
-  const bind = (typeof port === 'string') ? `Pipe ${port}` : `Port ${port}`;
-
-  switch(error.code) {
-
-    case 'EACCES':
-      console.error(`Error: ${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-
-    case 'EADDRINUSE':
-      console.error(`Error: ${bind} is already in use`);
-      process.exit(1);
-      break;
-
-    default:
-      throw error;
-  }
-});
-
-// Clean shutdown
-//
-const cleanShutdown = (shutdownType) => {
-  console.log(`[${shutdownType}] Initiating graceful shutdown...`);
-
-  // I ain't got time to wait, commenting this out but leaving it here for posterity.
+  // For the sake of clean code I'm not going out of my way to make these private
+  // beyond suggesting that they are private with the underscore-prefix naming convention.
   //
-  /*
-  httpServer.close(() => {
-    console.log('HTTP server closed remaining connections. Exiting now.');
-    process.exit(0);
-  });
-  */
-  process.exit(0);
-};
+  _initServer() {
 
-process.on('SIGTERM', () => cleanShutdown('SIGTERM'));
-process.on('SIGINT', () => cleanShutdown('SIGINT'));
+    this.httpServer = http.createServer(this.app).listen(port);
 
-module.exports = app; // for testing
+    this.httpServer.on('listening', () => {
+      console.log(`Welcome to ${appPackage.description} v${appPackage.version}`);
+      console.log(`Node ${process.version}, ${os.platform()}/${os.arch()}`);
+
+      const addr = this.httpServer.address();
+      const bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+
+      console.log(`HTTP server listening on ${bind} on ${os.hostname()}`);
+
+      this.httpServer.on('error', (error) => {
+        if(error.syscall !== 'listen') {
+          throw error;
+        }
+
+        const bind = (typeof port === 'string') ? `Pipe ${port}` : `Port ${port}`;
+
+        switch(error.code) {
+
+          case 'EACCES':
+            console.error(`Error: ${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+
+          case 'EADDRINUSE':
+            console.error(`Error: ${bind} is already in use`);
+            process.exit(1);
+            break;
+
+          default:
+            throw error;
+        }
+      });
+
+    });
+  }
+
+  _startWebSocketPublisher() {
+    WebSocketPublisher.instance().startPublishing(this.httpServer);
+  }
+
+  _initCleanShutdownHandler() {
+    const cleanShutdown = (shutdownType) => {
+      console.log(`[${shutdownType}] Initiating clean shutdown...`);
+
+      // We would actually want to destroy all current connections here, otherwise
+      // httpServer.close() will be wait until all current clients disconnect.
+      // Maybe later. In the meantime, commenting it out but leaving it here for posterity.
+      //
+      // Could be pedantic and also notify the app, cleanly shutdown DB connection, etc.
+      //
+      /*
+      this.httpServer.close(() => {
+        console.log('HTTP server closed remaining connections. Exiting now.');
+        process.exit(0);
+      });
+      */
+
+      process.exit(0);
+    };
+
+    process.on('SIGTERM', () => cleanShutdown('SIGTERM'));
+    process.on('SIGINT', () => cleanShutdown('SIGINT'));
+  }
+}
+
+module.exports = new WeatherStationServer().app; // exporting app for testing
